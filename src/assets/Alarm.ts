2b5642915ed70af1b9c9428c9b74b184aa0463fe
DESKTOP-DT8EIOG\wan2b <wan2b.lazy@gmail.com>
2022-10-12 21:10:00 +0900

    changeStandbyForAlarmとchangeAlarmNotificationの実装

--- src/assets/Alarm.ts
+++ src/assets/Alarm.ts
@@ -23,4 +23,26 @@ export class Alarm {
       `<h1>アラーム設定</h1><p><label for="interval">間隔（分）</label><input type="number" id="interval"><p><label for="scheduledToEnd">終了予定</label><input type="time" id="scheduledToEnd"></p><button id="startOfActivities">活動開始</button>`
     );
   }
+
+  changeStandbyForAlarm(element: HTMLDivElement) {
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
+
+    element.insertAdjacentHTML(
+      "afterbegin",
+      `<p>次回のアラームは<div></div><button>アラーム中断</button>`
+    );
+  }
+
+  changeAlarmNotification(element: HTMLDivElement) {
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
+
+    element.insertAdjacentHTML(
+      "afterbegin",
+      `<h1>ストレッチの時間です</h1><button>ストレッチ開始</button> <button>ストレッチ完了</button> <button>中断</button>`
+    );
+  }
 }
