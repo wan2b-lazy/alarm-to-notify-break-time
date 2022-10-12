42167e68b8215fc9961e3900ad0fc26dc8142724
DESKTOP-DT8EIOG\wan2b <wan2b.lazy@gmail.com>
2022-10-12 23:50:46 +0900

    mp3をwebpackで処理するための設定

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import * as audio from "./assets/alarm.mp3";
+import * as Alarm from "./assets/Alarm";
--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,10 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.mp3$/,
+        type: "asset/resource",
+      },
     ],
   },
 
