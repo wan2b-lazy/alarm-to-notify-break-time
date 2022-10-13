e4a66d92e1e49fab521cb017f0b7b9970a1936a5
DESKTOP-DT8EIOG\wan2b <wan2b.lazy@gmail.com>
2022-10-13 16:13:31 +0900

    ページ遷移用の関数をindex.tsへ移動

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,37 @@
 import audio from "./assets/alarm.mp3";
 import { Alarm } from "./assets/Alarm";
+
+let alarm: Alarm;
+
+function changeAlarmSetting(element: HTMLElement) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+
+  element.insertAdjacentHTML(
+    "afterbegin",
+    `<h1>アラーム設定</h1><p><label for="interval">間隔（分）</label><input type="number" id="interval"><p><label for="scheduledToEnd">終了予定</label><input type="time" id="scheduledToEnd"></p><button id="startOfActivities">活動開始</button>`
+  );
+}
+
+function changeStandbyForAlarm(element: HTMLElement) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+
+  element.insertAdjacentHTML(
+    "afterbegin",
+    `<p>次回のアラームは<div></div><button>アラーム中断</button>`
+  );
+}
+
+function changeAlarmNotification(element: HTMLElement) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+
+  element.insertAdjacentHTML(
+    "afterbegin",
+    `<h1>ストレッチの時間です</h1><button>ストレッチ開始</button> <button>ストレッチ完了</button> <button>中断</button>`
+  );
+}
