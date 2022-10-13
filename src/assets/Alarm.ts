8a5d09862d18a7489728b714bc7834d48c57edea
DESKTOP-DT8EIOG\wan2b <wan2b.lazy@gmail.com>
2022-10-13 21:23:54 +0900

    Alarmコンストラクタ 仮実装

--- src/assets/Alarm.ts
+++ src/assets/Alarm.ts
@@ -1,7 +1,23 @@
-export class Alarm {}
+import alarmFile from "./alarm.mp3";
+
+export class Alarm {
+  interval: number;
+  scheduledToEnd: number[] | undefined;
+  date: Date;
+  audio: HTMLAudioElement;
+
+  constructor(intervalInput: number, scheduledToEndInput: string) {
+    this.interval = intervalInput;
+    if (scheduledToEndInput != undefined) {
+      this.scheduledToEnd = scheduledToEndInput.split(":").map(Number);
+    }
+    this.date = new Date();
+    this.audio = new Audio(alarmFile);
+  }
+}
 
 export const changeSettingAlarm = (elem: HTMLElement) => {
-  console.log("changeSettingAlarm");
+  return new Alarm(1, "仮置き");
 };
 
 export const changeStandbyForAlarm = (elem: HTMLElement) => {
