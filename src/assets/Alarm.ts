e753ec4252a3540839c7fc51958d8220c395f25e
DESKTOP-DT8EIOG\wan2b <wan2b.lazy@gmail.com>
2022-10-15 13:43:52 +0900

    Alarm.calculateNextTimeの実装

--- src/assets/Alarm.ts
+++ src/assets/Alarm.ts
@@ -3,19 +3,36 @@ import alarmFile from "./alarm.mp3";
 export class Alarm {
   interval: number;
   scheduledToEnd: number[] | undefined;
+  nextTime: number[];
   date: Date;
   audio: HTMLAudioElement;
+  timerId: number;
 
   constructor(intervalInput: number, scheduledToEndInput: string | undefined) {
     this.interval = intervalInput;
-    if (scheduledToEndInput != null) {
+    if (scheduledToEndInput != undefined) {
       this.scheduledToEnd = scheduledToEndInput.split(":").map(Number);
     }
+    this.nextTime = [0, 0];
     this.date = new Date();
     this.audio = new Audio(alarmFile);
+    this.timerId = 0;
   }
 
-  setAlarm() {
-    console.log("setAlarm");
+  calculateNextTime() {
+    this.nextTime[0] = this.date.getHours() + Math.floor(this.interval / 60);
+    this.nextTime[1] = this.date.getMinutes() + (this.interval % 60);
+
+    if (this.nextTime[1] >= 60) {
+      const convertedMinute: number = Math.floor(this.nextTime[1] / 60);
+      this.nextTime[1] - convertedMinute * 60;
+      this.nextTime[0] + convertedMinute;
+    }
+
+    if (this.nextTime[0] >= 24) {
+      this.nextTime[0] - 24;
+    }
   }
+
+  setAlarm() {}
 }
