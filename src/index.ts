0d339cd65dfc9bbd0e799b6f6440b1f4a6977874
DESKTOP-DT8EIOG\wan2b <wan2b.lazy@gmail.com>
2022-10-16 00:14:30 +0900

    index.ts 大枠

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { audioFile } from "./assets/alarm.mp3";
+
+class Alarm {}
+const changeAlarmSetting = () => {};
+const changeStandbyAlarm = () => {};
+const changeAlarmNotification = () => {};
+
+changeAlarmSetting();
